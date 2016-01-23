f5769b689b677403c5d6df52dd184f59686461c9
Gail Matsushima <gail.matsushima@gmail.com>
2016-01-23 11:04:28 -1000

    removed console.log debugger

--- Public/js/states/boot.js
+++ Public/js/states/boot.js
@@ -1,14 +1,18 @@
 //class constructor
-//responsible for preloading assets and switching to the game state
+//responsible for ..
+//preloading assets
+//and switching to the game state
 
 ToeFu.Boot = function(){
 
 };
 
 ToeFu.Boot.prototype.preload = function() {
+  //will preload all assests
 
 };
 
 ToeFu.Boot.prototype.create = function() {
-
+  //switch to game state
+  this.state.start(ToeFu.STATES.GAME);
 };
