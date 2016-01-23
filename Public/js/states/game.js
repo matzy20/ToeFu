f4adb6d9dc1283a20566b306361cb2df44061e0e
Gail Matsushima <gail.matsushima@gmail.com>
2016-01-23 12:57:03 -1000

    got two players dancing

--- Public/js/states/game.js
+++ Public/js/states/game.js
@@ -1,21 +1,40 @@
-//class constructor
 //the Game State handles all sprites
+//IFFE wrap to create statics
+(function() {
 
-ToeFu.Game = function() {
+var INITIAL_POSITIONS = [
+  //player 1
+  {x : 100, y : 100},
+  //player 2
+  {x : 600, y : 100}
+];
+  //class constructor
+  ToeFu.Game = function() {
+    this.player_1;
+    this.player_2;
 
-};
+  };
 
-ToeFu.Game.prototype.create = function(){
-  //instantiates player and game
-  this.player_1 = new ToeFu.Player( this.game, 0 );
-  this.game.add.existing(this.player_1);
-};
+  ToeFu.Game.prototype.create = function(){
+    //instantiates player and game
+    this.player_1 = new ToeFu.Player( this.game, 0 );
+    this.player_2 = new ToeFu.Player( this.game, 1 );
+    this.game.add.existing(this.player_1);
+    this.game.add.existing(this.player_2);
 
-ToeFu.Game.prototype.update = function(){
+    //position players
+    this.player_1.x = INITIAL_POSITIONS[0].x;
+    this.player_1.y = INITIAL_POSITIONS[0].y;
+    this.player_2.x = INITIAL_POSITIONS[1].x;
+    this.player_2.y = INITIAL_POSITIONS[1].y;
+  };
 
-};
+  ToeFu.Game.prototype.update = function(){
 
-ToeFu.Game.prototype.shutdown = function() {
+  };
 
-};
+  ToeFu.Game.prototype.shutdown = function() {
 
+  };
+
+})();
